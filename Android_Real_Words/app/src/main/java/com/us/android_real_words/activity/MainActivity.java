e1d8c840527d836f769872a856b088724df16563
chenlixiong <chenlxhf@163.com>
2017-04-01 14:20:28 +0800

    project init

---
+++ Android_Real_Words/app/src/main/java/com/us/android_real_words/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.us.android_real_words.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.us.android_real_words.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
